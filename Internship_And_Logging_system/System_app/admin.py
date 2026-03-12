1c95cfee47ad1042c9db19510ade96331197427f
paul320 <paulacou4@gmail.com>
2026-03-12 19:42:35 +0300

    Update admin.py

--- Internship_And_Logging_system/System_app/admin.py
+++ Internship_And_Logging_system/System_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import CustomUser,InternshipPlacement,WeeklyLog,EvaluationCriteria,Evaluation
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(InternshipPlacement)
+admin.site.register(WeeklyLog)
+admin.site.register(EvaluationCriteria)
+admin.site.register(Evaluation)
