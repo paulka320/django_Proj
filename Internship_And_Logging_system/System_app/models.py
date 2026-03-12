cee02dc4b7f3d51a273c346831134a5036bf3266
paul320 <paulacou4@gmail.com>
2026-03-12 17:11:19 +0300

    Add CustomUser model with role choices

--- Internship_And_Logging_system/System_app/models.py
+++ Internship_And_Logging_system/System_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser,Group,Permission
 # Create your models here.
+class CustomUser(AbstractUser):
+  ROLE_CHOICES = (
+    ('student','student'),
+    ('academic_Supervisor','academic_Supervisor'),
+    ('supervisor','supervisor'),
+    ('adminstrator','adminstrator'),
+  )
+  role = Models.CharField(max_length=20,
+    
