4d1eb294c860a5ffff7ef538d85b2f50acc29bc4
paul320 <paulacou4@gmail.com>
2026-03-12 20:14:34 +0300

    Update models.py

--- Internship_And_Logging_system/System_app/models.py
+++ Internship_And_Logging_system/System_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 # Create your models here.
+
+
 class CustomUser(AbstractUser):
   ROLE_CHOICES = (
     ('student', 'student'),
