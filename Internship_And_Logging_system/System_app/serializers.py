67cd5776bbb7cd915f88a2e88658267f5f1e7d21
paulka320 <paulacou4@gmail.com>
2026-03-19 09:34:39 +0300

    updated serializers.py

--- Internship_And_Logging_system/System_app/serializers.py
+++ Internship_And_Logging_system/System_app/serializers.py
@@ -18,3 +18,14 @@ class CustomUserSerializer(serializers.Serializer):
         instance.save()
         return instance
     
+
+    class EvaluationCriteriaserializer(serializers.Serializer):
+        id = serializers.IntegerField(read_only = True)
+        name = serializers.CharField(max_length=50)
+        max_score = serializers.IntegerField()
+
+        def create(self,validated_data):
+            return EvaluationCriteria.objects.create(**validated_data)
+
+        def update(self,instance,validated_data):
+            instance.name = validated_data.get('username',instance.username)   
