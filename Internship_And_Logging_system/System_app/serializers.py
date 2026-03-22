33e07d624473664dd8f7d98710e1c64b72c8ce30
paulka320 <paulacou4@gmail.com>
2026-03-22 19:40:44 +0300

    updated serializers.py

--- Internship_And_Logging_system/System_app/serializers.py
+++ Internship_And_Logging_system/System_app/serializers.py
@@ -2,22 +2,8 @@ from rest_framework import serializers
 from .models import CustomUser,EvaluationCriteria,Evaluation,InternshipPlacement,WeeklyLog
 
 
-class CustomUserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only =True)
-    username =serializers.CharField(max_length=150)
-    email = serializers.EmailField()
-    role = serializers.CharField(max_length =30)
-
-    def create(self,validated_data):
-        return CustomUser.objects.create(**validated_data)
-    
-    def update(self,instance,validated_data):
-        instance.username = validated_data.get('username',instance.username)
-        instance.email = validated_data.get('email',instance.email)
-        instance.role = validated_data.get('role',instance.role)
-        instance.save()
-        return instance
-    
+class CustomUserSerializer(serializers.ModelSerializer):
+        
 
 class EvaluationCriteriaSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
