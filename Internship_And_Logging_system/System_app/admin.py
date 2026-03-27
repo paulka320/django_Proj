cc211254778213fe3f6012e949985a0b8ff2cfe2
nushifah <knushifah@gmail.com>
2026-03-28 01:44:52 +0300

    Enhance admin interface with list display for core models

--- Internship_And_Logging_system/System_app/admin.py
+++ Internship_And_Logging_system/System_app/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import CustomUser,InternshipPlacement,WeeklyLog,EvaluationCriteria,Evaluation
+from .models import CustomUser, InternshipPlacement, WeeklyLog, Evaluation, EvaluationCriteria
 # Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(InternshipPlacement)
-admin.site.register(WeeklyLog)
-admin.site.register(EvaluationCriteria)
-admin.site.register(Evaluation)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role')
+
+
+@admin.register(InternshipPlacement)
+class InternshipPlacementAdmin(admin.ModelAdmin):
+    list_display = ('student', 'company_name', 'start_date', 'end_date')
+
+
+@admin.register(WeeklyLog)
+class WeeklyLogAdmin(admin.ModelAdmin):
+    list_display = ('student', 'week_number', 'status')
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'criteria', 'score')
+
+
+@admin.register(EvaluationCriteria)
+class EvaluationCriteriaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'max_score')
